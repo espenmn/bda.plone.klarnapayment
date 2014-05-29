bff27ac7ba2597a0e7bb7648c1a3990ed6fd0434
Espen Moe-Nilssen <espen@medialog.no>
2014-05-29 17:06:39 +0200

    Added settings to control panel

--- src/bda/plone/klarnapayment/__init__.py
+++ src/bda/plone/klarnapayment/__init__.py
@@ -1,1 +1,27 @@
-#
+from bda.plone.shop import message_factory as _
+
+from zope import schema
+from plone.supermodel import model
+from zope.interface import Interface
+from zope.interface import Attribute
+from zope.interface import provider
+
+from bda.plone.shop.interfaces import IShopSettingsProvider
+
+@provider(IShopSettingsProvider)
+class IKlarnaPaymentSettings(model.Schema):
+    
+    model.fieldset( 'klarna',label=_(u'Klarna', default=u'Klarna'),
+        fields=[
+        'klarna_eid',
+        'klarna_secret',
+        ],
+    )
+                   
+    klarna_eid = schema.TextLine(title=_(u'eid', default=u'Eid'),
+                 required=True
+    )
+
+    klarna_secret = schema.TextLine(title=_(u'secret', default=u'Shared Secret'),
+               required=True
+    )
